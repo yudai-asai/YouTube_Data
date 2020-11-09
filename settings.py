abaf12a95d61d657a695d7898809b16be84e13af
EC2 Default User <ec2-user@ip-172-31-63-6.ec2.internal>
2020-11-09 17:05:33 +0000

    Env Done

--- settings.py
+++ settings.py
@@ -8,5 +8,5 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 YouTube_API_KEY = os.environ.get("YouTube_API_KEY")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
+YouTube_CHANNEL_ID = os.environ.get("CHANNEL_ID")
 SLACK_URL = os.environ.get("SLACK_URL")
